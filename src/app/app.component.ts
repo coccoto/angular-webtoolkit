b9d647fc2b933b1308f357536561ad950844e402
coccoto <soprano.lune@gmail.com>
2024-01-02 05:08:31 +0900

    2024-01-02 05:08:31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+// component
+import { BaseconverterComponent } from './baseconverter/baseconverter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, BaseconverterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'webtoolkit';
