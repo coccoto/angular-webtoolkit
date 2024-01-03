37fa4097dde4768e0d3836051adba21b9ed8d80d
coccoto <soprano.lune@gmail.com>
2024-01-04 01:08:03 +0900

    2024-01-04 01:08:01

---
+++ client/.angular/cache/17.0.8/vite/deps_temp_5c0a4841/@angular_platform-browser_animations.js
@@ -0,0 +1,21 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-PX2CVKC6.js";
+import "./chunk-DXTM26VX.js";
+import "./chunk-YXAGQBKD.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-QOH6KD3I.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
