0b81b9f3f52bf9bf122d4a76fe4513524e68754a
coccoto <soprano.lune@gmail.com>
2024-01-11 01:44:48 +0900

    2024-01-11 01:44:48

--- client/src/app/shared/header/header.component.ts
+++ client/src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Renderer2, ElementRef } from '@angular/core';
 // context
 import { AppContextService } from '@src/app/app.context.service'
 // @angular/material
@@ -20,17 +20,34 @@ import { MatSidenav } from '@angular/material/sidenav';
 export class HeaderComponent {
   @Input() menu: MatSidenav | undefined
 
-  constructor (private appContextService: AppContextService) { }
+  constructor (
+    private renderer2: Renderer2,
+    private elementRef: ElementRef,
+    private appContextService: AppContextService,
+  ) { }
 
   ngOnInit() {
-    if (this.menu !== undefined && this.appContextService.isPc) {
-      this.menu.toggle()
+    if (this.appContextService.isPc) {
+      this.handleMenuClick()
     }
   }
 
   handleMenuClick(): void {
     if (this.menu !== undefined) {
       this.menu.toggle()
+    }
+  }
+
+  handleContrastClick(): void {
+    const elemBody = this.elementRef.nativeElement.ownerDocument.body
+
+    if (elemBody.classList.contains('light-theme')) {
+      this.renderer2.addClass(elemBody, 'dark-theme')
+      this.renderer2.removeClass(elemBody, 'light-theme')
+
+    } else {
+      this.renderer2.addClass(elemBody, 'light-theme')
+      this.renderer2.removeClass(elemBody, 'dark-theme')
     }
   }
 }
